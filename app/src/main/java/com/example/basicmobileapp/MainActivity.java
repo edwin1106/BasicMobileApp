353deef6ed3634af4be263d8daa8912fe6d7d681
Jhaideee0123 <jhon.betancur@ceiba.com.co>
2020-08-25 19:36:16 -0500

    Se agregan funcionalidades para dispositivos móviles

--- app/src/main/java/com/example/basicmobileapp/MainActivity.java
+++ app/src/main/java/com/example/basicmobileapp/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.basicmobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button buttonPresentation;
+    Button buttonForm;
+    Button buttonCalculator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonPresentation = (Button) findViewById(R.id.buttonPresentation);
+        buttonForm = (Button) findViewById(R.id.buttonForm);
+        buttonCalculator = (Button) findViewById(R.id.buttonCalculator);
+
+        buttonPresentation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, PresentationActivity.class);
+                startActivity(i);
+            }
+        });
+
+        buttonForm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, FormActivity.class);
+                startActivity(i);
+            }
+        });
+
+        buttonCalculator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, CalculatorActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
