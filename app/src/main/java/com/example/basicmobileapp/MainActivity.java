909ade17855b94bea3f5e653f3ab721176698e93
edwin <edwinosoriogaviria@hotmail.com>
2020-08-26 18:34:13 -0500

    Added iternationalization

--- app/src/main/java/com/example/basicmobileapp/MainActivity.java
+++ app/src/main/java/com/example/basicmobileapp/MainActivity.java
@@ -6,20 +6,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button buttonPresentation;
-    Button buttonForm;
-    Button buttonCalculator;
+    private Button buttonPresentation;
+    private Button buttonForm;
+    private Button buttonCalculator;
+    private TextView teamPresentation;
+    private TextView ageCalculator;
+    private TextView form;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        teamPresentation = findViewById(R.id.teamPresentation);
+        teamPresentation.setText(R.string.team_presentation);
+
+        ageCalculator = findViewById(R.id.ageCalculator);
+        ageCalculator.setText(R.string.age_calculator);
+
+        form = findViewById(R.id.form);
+        form.setText(R.string.form);
+
         buttonPresentation = (Button) findViewById(R.id.buttonPresentation);
+        buttonPresentation.setText(R.string.text_button);
+
         buttonForm = (Button) findViewById(R.id.buttonForm);
+        buttonForm.setText(R.string.text_button);
+
         buttonCalculator = (Button) findViewById(R.id.buttonCalculator);
+        buttonCalculator.setText(R.string.text_button);
 
         buttonPresentation.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/basicmobileapp/PresentationActivity.java
+++ app/src/main/java/com/example/basicmobileapp/PresentationActivity.java
@@ -3,12 +3,22 @@ package com.example.basicmobileapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PresentationActivity extends AppCompatActivity {
+
+    private TextView nameMember1;
+    private TextView nameMember2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_presentation);
+        nameMember1 = findViewById(R.id.nameMember1);
+        nameMember1.setText(R.string.name_member1);
+
+        nameMember2 = findViewById(R.id.nameMember2);
+        nameMember2.setText(R.string.name_member2);
     }
 }
